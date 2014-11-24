5565ae8a84d7cbeeb153238da0b60d6ae64928a3
theapi <petermclarke@gmail.com>
2014-11-24 18:40:13 +0000

    Run rainbow sequence on command from the controller

--- Controller/Controller/Controller.ino
+++ Controller/Controller/Controller.ino
@@ -1,10 +1,5 @@
-/*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
-//2014 - TMRh20 - Updated along with Optimized RF24 Library fork
+/**
+ * Arduino controller for the mood lights.
  */
 
 /**
@@ -30,108 +25,69 @@ RF24 radio(7,8);
 byte addresses[][6] = {"1Node","2Node"};
 
 
-// Set up roles to simplify testing 
-boolean role;                                    // The main role variable, holds the current role identifier
-boolean role_ping_out = 1, role_pong_back = 0;   // The two different roles.
-
-void setup() {
-
-
+void setup() 
+{
   Serial.begin(57600);
   printf_begin();
   printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
+  printf("*** Enter an INT then hit return to transmit it to the other node\n\r");
 
   // Setup and configure rf radio
   radio.begin();                          // Start up the radio
   radio.setAutoAck(1);                    // Ensure autoACK is enabled
   radio.setRetries(15,15);                // Max delay between retries & number of retries
-  radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1,addresses[0]);
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
   
   radio.startListening();                 // Start listening
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
 }
 
-void loop(void){
+void loop(void)
+{
+
+  // if there's any serial available, read it:
+  while (Serial.available() > 0) {
+
+    radio.stopListening(); // First, stop listening so we can talk.
     
+    // look for the next valid integer in the incoming serial stream:
+    int send_val = Serial.parseInt(); 
 
-/****************** Ping Out Role ***************************/
-  if (role == role_ping_out)  {
-    
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
-    printf("Now sending \n\r");
-
-    unsigned long time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &time, sizeof(unsigned long) )){  printf("failed.\n\r");  }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-        
-    if ( timeout ){                                             // Describe the results
-        printf("Failed, response timed out.\n\r");
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-
-        // Spew it
-        printf("Sent %lu, Got response %lu, round-trip delay: %lu microseconds\n\r",time,got_time,micros()-got_time);
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-/****************** Pong Back Role ***************************/
-
-  if ( role == role_pong_back )
-  {
-    if( radio.available()){
-      unsigned long got_time;                                       // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }    
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      printf("Sent response %lu \n\r", got_time);  
-   }
- }
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == role_pong_back )
-    {
-      printf("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK\n\r");
-
-      role = role_ping_out;                  // Become the primary transmitter (ping out)
-      radio.openWritingPipe(addresses[0]);
-      radio.openReadingPipe(1,addresses[1]);
-  
-    }
-    else if ( c == 'R' && role == role_ping_out )
-    {
-      printf("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK\n\r");
+    // look for the newline.
+    if (Serial.read() == '\n') {
+      Serial.print(send_val);
+      printf("Now sending \n\r");
       
-       role = role_pong_back;                // Become the primary receiver (pong back)
-       radio.openWritingPipe(addresses[1]);
-       radio.openReadingPipe(1,addresses[0]);
+      //unsigned long time = micros(); // Take the time, and send it.  This will block until complete     
+      if (!radio.write( &send_val, sizeof(int) )){  printf("failed.\n\r");  }
+      
+      radio.startListening();                                    // Now, continue listening
+
+      unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+      boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+      
+      while ( ! radio.available() ) {                             // While nothing is received
+        if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+            timeout = true;
+            break;
+        }      
+      }
+          
+      if ( timeout ) {                                             // Describe the results
+          printf("Failed, response timed out.\n\r");
+      } else {
+          unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
+          radio.read( &got_time, sizeof(unsigned long) );
+    
+          // Spew it
+          printf("Sent %lu, Got response %lu, round-trip delay: %lu microseconds\n\r",send_val,got_time,micros()-got_time);
+      }
+      
+      
     }
   }
+  
+
 }
+
--- Light/Light.ino
+++ Light/Light.ino
@@ -1,17 +1,4 @@
 /*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-version 2 as published by the Free Software Foundation.
-
-    rf24ping85.ino by tong67 ( https://github.com/tong67 )
-    This is an example of how to use the RF24 class to communicate with ATtiny85 and other node.
-    Write this sketch to an ATtiny85. It will act like the 'transmit' mode of GettingStarted.ino
-    Write GettingStarted.ino sketch to UNO (or other board or RPi) and put the node in 'receiver' mode.
-    The ATtiny85 will transmit a counting number every second starting from 1.
-    The ATtiny85 uses the tiny-core by CodingBadly (https://code.google.com/p/arduino-tiny/)
-    When direct use of 3v3 does not work (UNO boards have bad 3v3 line) use 5v with LED (1.8V ~ 2.2V drop)
-    For low power consumption solutions floating pins (SCK and MOSI) should be pulled high or low with eg. 10K
-
     ** Hardware configuration **
     ATtiny25/45/85 Pin map with CE_PIN 3 and CSN_PIN 4
                                  +-\/-+
@@ -21,39 +8,11 @@ version 2 as published by the Free Software Foundation.
     nRF24L01 GND, pin1 --- GND  4|    |5  PB0 --- nRF24L01 MISO, pin6
                                  +----+
 
-    ATtiny25/45/85 Pin map with CE_PIN 3 and CSN_PIN 3 => PB3 and PB4 are free to use for application
-    Circuit idea from http://nerdralph.blogspot.ca/2014/01/nrf24l01-control-with-3-attiny85-pins.html
-    Original RC combination was 1K/100nF. 22K/10nF combination worked better.
-	For best settletime delay value in RF24::csn() the timingSearch3pin.ino scatch can be used.
-    This configuration is enabled when CE_PIN and CSN_PIN are equal, e.g. both 3
-    Because CE is always high the power consumption is higher than for 5 pins solution
-                                                                                            ^^
-                                 +-\/-+           nRF24L01   CE, pin3 ------|              //
-                           PB5  1|o   |8  Vcc --- nRF24L01  VCC, pin2 ------x----------x--|<|-- 5V
-                   NC      PB3  2|    |7  PB2 --- nRF24L01  SCK, pin5 --|<|---x-[22k]--|  LED
-                   NC      PB4  3|    |6  PB1 --- nRF24L01 MOSI, pin7  1n4148 |
-    nRF24L01 GND, pin1 -x- GND  4|    |5  PB0 --- nRF24L01 MISO, pin6         |
-                        |        +----+                                       |
-                        |-----------------------------------------------||----x-- nRF24L01 CSN, pin4 
-                                                                       10nF
-
-    ATtiny24/44/84 Pin map with CE_PIN 8 and CSN_PIN 7
-	Schematic provided and successfully tested by Carmine Pastore (https://github.com/Carminepz)
-                                  +-\/-+
-    nRF24L01  VCC, pin2 --- VCC  1|o   |14 GND --- nRF24L01  GND, pin1
-                            PB0  2|    |13 AREF
-                            PB1  3|    |12 PA1
-                            PB3  4|    |11 PA2 --- nRF24L01   CE, pin3
-                            PB2  5|    |10 PA3 --- nRF24L01  CSN, pin4
-                            PA7  6|    |9  PA4 --- nRF24L01  SCK, pin5
-    nRF24L01 MOSI, pin7 --- PA6  7|    |8  PA5 --- nRF24L01 MISO, pin6
-                                  +----+
 */
 
 // CE and CSN are configurable, specified values for ATtiny85 as connected above
 #define CE_PIN 3
 #define CSN_PIN 4
-//#define CSN_PIN 3 // uncomment for ATtiny85 3 pins solution
 
 #include <Adafruit_NeoPixel.h>
 
@@ -65,54 +24,44 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
 
 RF24 radio(CE_PIN, CSN_PIN);
 
-byte addresses[][6] = {
-  "1Node","2Node"};
-unsigned long payload = 0;
+byte addresses[][6] = {"1Node","2Node"};
 
-void setup() {
-  
+void setup() 
+{
   strip.begin();
+  strip.setPixelColor(0, 0, 0, 0); // Off (only one NeoPixel)
   strip.show(); // Initialize all pixels to 'off'
   
   // Setup and configure rf radio
   radio.begin(); // Start up the radio
   radio.setAutoAck(1); // Ensure autoACK is enabled
   radio.setRetries(15,15); // Max delay between retries & number of retries
-  radio.openWritingPipe(addresses[0]); // Write to device address '1Node'
-  radio.openReadingPipe(1,addresses[1]); // Read on pipe 1 for device address '2Node'
+  radio.openWritingPipe(addresses[1]); // "ping back"
+  radio.openReadingPipe(1,addresses[0]);
   radio.startListening(); // Start listening
 }
 
-void loop(void){
+void loop(void)
+{
   
-  radio.stopListening(); // First, stop listening so we can talk.
-  
-  pinMode(PIN, OUTPUT);
-  rainbow(10);
-  pinMode(PIN, INPUT);
-  
-  payload++;
-  radio.write( &payload, sizeof(unsigned long) );
-  radio.startListening(); // Now, continue listening
-
-    unsigned long started_waiting_at = micros(); // Set up a timeout period, get the current microseconds
-  boolean timeout = false; // Set up a variable to indicate if a response was received or not
-
-  while ( !radio.available() ){ // While nothing is received
-    if (micros() - started_waiting_at > 200000 ){ // If waited longer than 200ms, indicate timeout and exit while loop
-      timeout = true;
-      break;
-    }
-
+  // Check for a message from the controller
+  if (radio.available()) {
+    int got_val;       // Variable for the received timestamp
+    unsigned long time = micros();
+    //while (radio.available()) {                      // While there is data ready
+      radio.read( &got_val, sizeof(int) );             // Get the payload
+    //} 
+    radio.stopListening();  
+    radio.writeFast( &time, sizeof(unsigned long) );
+    
+    pinMode(PIN, OUTPUT);
+    rainbow(100);
+    strip.setPixelColor(0, 0, 0, 0); // Off (only one NeoPixel)
+    strip.show();
+    pinMode(PIN, INPUT);
+    
+    radio.startListening();
   }
-
-  if ( !timeout ){ // Describe the results
-    unsigned long got_time; // Grab the response, compare, and send to debugging spew
-    radio.read( &got_time, sizeof(unsigned long) );
-  }
-
-  // Try again 1s later
-  delay(1000);
 }
 
 void rainbow(uint8_t wait) {
