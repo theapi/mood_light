ace862a9853c04b3d76f5e1f5e38ff6626248376
theapi <petermclarke@gmail.com>
2014-12-06 13:52:49 +0000

    Start of individual addresses

--- Atmega_Light/Atmega_Light.ino
+++ Atmega_Light/Atmega_Light.ino
@@ -1,4 +1,9 @@
 
+//#define RX_ADDRESS "2Node"
+//#define RX_ADDRESS "3Node"
+#define RX_ADDRESS "4Node"
+//#define RX_ADDRESS "5Node"
+//#define RX_ADDRESS "6Node"
 
 #include <SPI.h>
 #include "nRF24L01.h"
@@ -8,8 +13,8 @@
 #define PIN_CE  7
 #define PIN_CSN 8
  
-
-byte addresses[][6] = {"1Node","2Node"};
+// The address that this node listens on
+byte address[6] = RX_ADDRESS;
 
 RF24 radio(PIN_CE, PIN_CSN);
 
@@ -17,17 +22,15 @@ void setup()
 {
   Serial.begin(57600);
   printf_begin();
-  printf("\n\r RF24_Receiver \n\r");
-  
+  printf("\n\r RF24_Receiver on address: %s \n\r", RX_ADDRESS);
 
-  
   // Setup and configure rf radio
   radio.begin(); // Start up the radio
   radio.setPayloadSize(2);                // Only two byte payload gets sent (int on arduino) (short on 32bit rpi)
   radio.setAutoAck(1); // Ensure autoACK is enabled
   radio.setRetries(15,15); // Max delay between retries & number of retries
-  radio.openWritingPipe(addresses[1]); // "ping back"
-  radio.openReadingPipe(1,addresses[0]);
+
+  radio.openReadingPipe(1, address);
   radio.startListening(); // Start listening
   
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
